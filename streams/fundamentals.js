2a34152a4c844262db66b495fa2509b896ae3172
Edmar Junior <59017508+edmar229@users.noreply.github.com>
2023-11-23 17:09:47 -0300

    Criando uma stream de leitura

---
+++ streams/fundamentals.js
@@ -0,0 +1,23 @@
+//Criando uma stream do zero
+import { Readable } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+  index = 1
+
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i > 100) {
+        //Quando envia null a stream deve parar
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+  
+        this.push(buf)
+      }
+    }, 1000)
+  }
+}
+
+new OneToHundredStream().pipe(process.stdout)
